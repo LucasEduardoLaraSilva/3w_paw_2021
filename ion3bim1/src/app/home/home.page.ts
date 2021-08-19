9ccf73eb73227b88792242df7d4984598b62c7b5
LucasEduardoLaraSilva <lucaseduaedo644@gmail.com>
2021-08-19 19:05:33 +0000

    Começo de um projeto

--- ion3bim1/src/app/home/home.page.ts
+++ ion3bim1/src/app/home/home.page.ts
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+num1 = null;
+num2 = null;
+resposta = null;
+
   constructor() {}
+
+  calcular() void{
+    this.resposta = this.num1 + this.num2;
+  }
 
 }
