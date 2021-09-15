64f8f3f7bbfb6863dd2f5d6615233632e4825c0b
LucasEduardoLaraSilva <lucaseduaedo644@gmail.com>
2021-09-15 13:56:33 -0300

    Correção projeto Combustível

--- ion3bim1/src/app/home/home.page.ts
+++ ion3bim1/src/app/home/home.page.ts
@@ -6,36 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  gasolina  = undefined;
-  etanol    = undefined;
-  resposta  = undefined;
+  imagens = [
+    'inicio.jpg',
+    'etanol.jpg',
+    'gasolina.jpg',
+    'dois.jpg'
+  ];
+  imagem = this.imagens[0];
+  gasolina = undefined;
+  etanol = undefined;
 
- combustivel = [
-   'gasolina.jpg',
-   'etanol.jpg',
-   'dois.jpg'
- ]
- inicio = 'inicio.png';
-  
+ 
   constructor() {}
 
-  trocarCombustivel(indice: number):void{
-    this.inicio = this.combustivel[indice];
-  }
+  verificar(): void{
+    let resp =  this.etanol / this.gasolina;
 
-  verificar():void{
-    let diferenca = this.etanol / this.gasolina;
-
-    if(diferenca > 0.7){
-      this.inicio = this.combustivel[0];
-      this.resposta = 'gasolina';
-    }else if(diferenca < 0.7){
-      this.inicio = this.combustivel[1];
-      this.resposta = 'etanol';
+    if(resp < 0.7){
+      this.imagem = this.imagens[1];
+    }else if(resp > 0.7){
+      this.imagem = this.imagens[2];
     }else{
-      this.inicio = this.combustivel[2];
-      this.resposta = 'Tanto faz';
-    }
+      this.imagem = this.imagens[3];
+        }
+  }
+  
   }
 
-}
